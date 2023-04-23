0c4d9013626c6d98b7a68a85aa9f6e16767fede4
MEX1ME <master-fond@mail.ru>
2023-04-23 16:45:08 +0400

    HW1 add task4

--- Homeworks/HW1/Program.cs
+++ Homeworks/HW1/Program.cs
@@ -50,15 +50,51 @@
 
 //  Task3. Напишите программу, которая на вход принимает число и выдает, является ли число четным.
 
-Console.WriteLine("please put you number here");
-int num = Convert.ToInt32(Console.ReadLine());
-int remDiv = num % 2;
-if(remDiv == 0)
+// Console.WriteLine("please put you number here");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int remDiv = num % 2;
+// if(remDiv == 0)
+// {
+//     Console.WriteLine($"Your number is even"); //четный
+// }
+// else
+// {
+//     Console.WriteLine($"Your number is odd"); //нечетный
+// }
+
+//  Task4. Напишите программу, которая принимает на вход число (N), а на выходе показывает все четные числа от 1 до N.
+//  Вместо проверки остатка от деления на 2, можно было идти до нужного числа шагами, равными "2" по четным числам.
+Console.WriteLine("Enter you number");
+int n = Convert.ToInt32(Console.ReadLine());
+int point = 1;
+if (n == 1)
 {
-    Console.WriteLine($"Your number is even"); //четный
+    Console.WriteLine("Congratulations, you are the captain \"mathematic\"!");
 }
 else
 {
-    Console.WriteLine($"Your number is odd"); //нечетный
+    Console.WriteLine("You see all even numbers from 1 to you number");
+    if (point < n)
+    {
+        while (point <= n)
+        {
+            if (point % 2 == 0)
+            {
+                Console.WriteLine(point);
+            }
+            point++;
+        }
+    }
+    else if (point > n)
+    {
+        while (point >= n)
+        {
+            if (point % 2 == 0)
+            {
+                Console.WriteLine(point);
+            }
+            point--;
+        }
+    }
 }
 
