2d26d5d4f310b17005e2e3ce2535ebfee9f7c4b3
MEX1ME <master-fond@mail.ru>
2023-04-30 01:35:29 +0400

    add hw2

---
+++ Homeworks/HW2/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+// int Search2Number(int a)
+// {
+//     int b = (a % 100) / 10;
+//     return b;
+// }
+
+// Console.WriteLine($"please enter your three digit number");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// if (a / 100 >= 1 && a / 100 <= 9)
+// {
+//     int c = Search2Number(a);
+//     Console.WriteLine(c);
+// }
+// else
+// {
+//     Console.WriteLine($"the number is not three-digit");
+// }
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+// int Find3num(int num)
+// {
+//     int a = 100;
+//     int ost = -1;
+//     while (ost < 0)
+//     {
+//         if (num >= a && num < 10 * a)
+//         {
+//             ost = (num % (a / 10)) / (a / 100);
+//         }
+//         else
+//         {
+//             a = a * 10;
+//         }
+//     }
+//     return ost;
+// }
+
+// Console.WriteLine("enter a number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num < 0) num = num * -1;
+// if (num < 100) Console.WriteLine("there is no third digit");
+// else Console.WriteLine(Find3num(num));
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// bool CheckDay(int a)
+// {
+//     if (a == 6 || a == 7) return true;
+//     else return false;
+// }
+// Console.WriteLine("enter the day number");
+// int dayNumber = Convert.ToInt32(Console.ReadLine());
+// if (dayNumber > 0 && dayNumber < 8) Console.WriteLine($"\"Day of week № {dayNumber} is a weekend\" - {CheckDay(dayNumber)}");
+// else Console.WriteLine($"{dayNumber} is not the number of the day of the week");
---
+++ Seminars/Seminar3/Program.cs
@@ -0,0 +1,73 @@
+﻿//Task1 Найти номер четверти
+
+// int FindQuard(int x, int y)
+// {
+//     if (x > 0 && y > 0) return 1;
+//     if (x < 0 && y > 0) return 2;
+//     if (x < 0 && y < 0) return 3;
+//     if (x > 0 && y < 0) return 4;
+
+//     return 0;
+// }
+
+// Console.WriteLine("input X:");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("input Y:");
+// int y = Convert.ToInt32(Console.ReadLine());
+// int result = FindQuard(x, y);
+
+// Console.WriteLine($"Point with {x} and {y} coordinates located at {result} quart");
+
+//Task3 Найти расстояние между точками плоскости
+
+// double FindDistance (double xa, double ya, double xb, double yb)
+// {
+//     return Math.Sqrt(Math.Pow(xb-xa, 2) + Math.Pow(yb-ya, 2));
+// }
+
+// Console.WriteLine("Input coordinate x to A:");
+// double xa = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine("Input coordinate y to A:");
+// double ya = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine("Input coordinate x to B:");
+// double xb = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine("Input coordinate y to B:");
+// double yb = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine($"Distance between point A with coordinate ({xa}, {ya}) and point B with coordinates ({xb},{yb}) -> {FindDistance (xa,ya,xb,yb)}");
+
+//Task3 По заданному номеру четверти показать диапазон возможных координат точек в этой четверти (x и y)
+
+// string Diapazon(int num)
+// {
+//     if (num == 1) return "x>0, y>0";
+//     if (num == 2) return "x<0, y>0";
+//     if (num == 3) return "x<0, y<0";
+//     if (num == 4) return "x>0, y<0";
+//     return "0";
+// }
+// Console.WriteLine("Input quard");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"quard = {num}, coordinate {Diapazon(num)}");
+
+//Task Программа, которая принимает на вход число N и возвращает квадрат чисел от 1 до N]
+
+void XpeHb(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count} -> {Math.Pow(count, 2)}");
+        count++;
+    }
+}
+
+Console.WriteLine("Input num");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 0) Console.WriteLine($"input correct num");
+else
+XpeHb(num);
